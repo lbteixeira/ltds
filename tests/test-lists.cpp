4e409f56b6d93092c8385f85546337b1c022dabe
Lucas Teixeira <lucasteixeira.ufmg@gmail.com>
2021-11-07 20:05:50 +0100

    tests for singly linked lists

--- tests/test-lists.cpp
+++ tests/test-lists.cpp
@@ -1,11 +1,10 @@
 #include <catch2/catch.hpp>
-#include "ltds/node.hpp"
+#include "ltds/lists.hpp"
 
-TEST_CASE("Construction of nodes", "[node]"){
-  ltds::node<float> node1;
-  ltds::node<float> node2;
-  ltds::node<int> node3;
-  ltds::node<int> node4;
+TEST_CASE("Construction of nodes", "[node]"){ ltds::nodeSingle<float> node1;
+  ltds::nodeSingle<float> node2;
+  ltds::nodeSingle<int> node3;
+  ltds::nodeSingle<int> node4;
 
   node1.key = 1.0;
   node2.key = 2.0;
@@ -22,4 +21,48 @@ TEST_CASE("Construction of nodes", "[node]"){
   REQUIRE(node2.next == &node1);
   REQUIRE(node3.next == &node4);
   REQUIRE(node4.next == &node3);
+}
+
+TEST_CASE("Singly linked lists", "[slist]"){
+  const int intArray[] = {7, 10, 4, 13};
+  const double doubleArray[] = {7.0, 10.0, 4.0, 13.0};
+  ltds::singlyLinkedList<int> listInt1;
+  ltds::singlyLinkedList<int> listInt2(intArray);
+  ltds::singlyLinkedList<double> listDouble1;
+  ltds::singlyLinkedList<double> listDouble2(doubleArray);
+
+  SECTION("Initial state"){
+    REQUIRE(listInt1.head.next == nullptr);
+    REQUIRE(listInt2.head.next == nullptr);
+    REQUIRE(listDouble1.head.next == nullptr);
+    REQUIRE(listDouble2.head.next == nullptr);
+
+    REQUIRE(listInt1.tail.next == nullptr);
+    REQUIRE(listInt2.tail.next == nullptr);
+    REQUIRE(listDouble1.tail.next == nullptr);
+    REQUIRE(listDouble2.tail.next == nullptr);
+  };
+
+  SECTION("Push Front"){
+    int keyInt = 26;
+    double keyDouble = 26.0;
+    listInt1.pushFront(keyInt);
+    listDouble1.pushFront(keyDouble);
+
+    REQUIRE(listInt1.head.next->key == 26);
+    REQUIRE(listDouble1.head.next->key == 26.0);
+
+    listInt1.pushFront(12);
+    listDouble1.pushFront(12.0);
+
+    REQUIRE(listInt1.head.next->key == 12);
+    REQUIRE(listDouble1.head.next->key == 12.0);
+  };
+
+  SECTION("Pop Front"){
+    listInt1.pushFront(10);
+    listInt1.pushFront(26);
+    listInt1.popFront();
+    REQUIRE(listInt1.head.next->key == 10);
+  };
 }
